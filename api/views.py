e9b5c2ebda79df04669de19f80457e62b1f37260
Daniel <huntadonis@gmail.com>
2023-01-28 00:29:58 +0100

    httponly removed

--- api/views.py
+++ api/views.py
@@ -251,7 +251,7 @@ class LoginView(APIView):
 
             response = Response()
 
-            response.set_cookie(key='jwt', value=token, httponly=True)
+            response.set_cookie(key='jwt', value=token, httponly=False)
 
             response.data = {
                 'jwt': token,
